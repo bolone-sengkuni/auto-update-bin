4e6516d5e5776e1af278096d17099290475dd251
bolone-sengkuni <toyib30071999@gmail.com>
2025-05-15 01:27:27 +0700

    update run-main.yml and main.py

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ TOKEN = os.getenv("GITHUB_TOKEN")
 
 os.makedirs('bin', exist_ok=True)
 
+with open('bin/.gitkeep', 'w') as f:
+    f.write("#")
+
 
 def get_release_new(owner: str, repo: str):
     resp = req.get(
